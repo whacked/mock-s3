fb777b806ef31512205462472136d8a61eaa7d86
natto <natto@natto-tp.(none)>
2012-05-31 19:54:14 -0400

    import OrdinaryCallingFormat directoy to save a line

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
 import boto
+from boto.s3.connection import OrdinaryCallingFormat
 from boto.s3.key import Key
 
-s3 = boto.connect_s3(host='localhost', port=10001, is_secure=False)
-# boto.s3.connection doesn't seem to become available
-# until after you attempt a connection
-calling_format = boto.s3.connection.OrdinaryCallingFormat()
 s3 = boto.connect_s3(host='localhost', port=10001, is_secure=False,
-        calling_format = calling_format)
+        calling_format = OrdinaryCallingFormat())
 
 
 dc_test_content = {}
